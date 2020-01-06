a9adba84ed2de0fa5af559fd66c7ab23d7317bdf
Lin Chen <clin@thoughtworks.com>
2020-01-06 17:58:59 +0800

    [MovieService][Chen] add movie service get user info from user service

---
+++ movie-service/src/main/java/com/clin/sample/controller/MovieController.java
@@ -0,0 +1,21 @@
+package com.clin.sample.controller;
+
+import com.clin.sample.model.User;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class MovieController {
+
+	@Autowired
+	private RestTemplate restTemplate;
+
+	@GetMapping("/user/{id}")
+	public User findById(@PathVariable("id") Long id) {
+		return this.restTemplate.getForObject("http://localhost:8000/" + id, User.class);
+	}
+}
