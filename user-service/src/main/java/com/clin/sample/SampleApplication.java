11aef3cd4e526f017f98a3d0832ae3c51a3a5111
Lin Chen <lc1990linux@gmail.com>
2020-01-07 12:04:12 +0800

    [UserService][Chen] register userservice to eureka

--- user-service/src/main/java/com/clin/sample/SampleApplication.java
+++ user-service/src/main/java/com/clin/sample/SampleApplication.java
@@ -1,9 +1,11 @@
 package com.clin.sample;
 
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class SampleApplication {
 
 	public static void main(String[] args) {
