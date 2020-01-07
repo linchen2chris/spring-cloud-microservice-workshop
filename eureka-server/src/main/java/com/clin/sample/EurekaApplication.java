ade8b1deeb1b21cc77e35b76809c233a0bc0b061
Lin Chen <lc1990linux@gmail.com>
2020-01-07 11:19:00 +0800

    [EurekaService][Chen] init eureka server

---
+++ eureka-server/src/main/java/com/clin/sample/EurekaApplication.java
@@ -0,0 +1,15 @@
+package com.clin.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaApplication.class, args);
+	}
+
+}
