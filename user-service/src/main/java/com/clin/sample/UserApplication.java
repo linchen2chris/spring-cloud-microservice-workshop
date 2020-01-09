b2e901d6974e71f43195237f9e5a02a4fd83c4af
Lin Chen <lc1990linux@gmail.com>
2020-01-09 13:09:24 +0800

    [UserService][Chen] fix user service lsp not working

--- user-service/src/main/java/com/clin/sample/UserApplication.java
+++ user-service/src/main/java/com/clin/sample/UserApplication.java
@@ -6,10 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class SampleApplication {
+public class UserApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SampleApplication.class, args);
+		SpringApplication.run(UserApplication.class, args);
 	}
 
 }
