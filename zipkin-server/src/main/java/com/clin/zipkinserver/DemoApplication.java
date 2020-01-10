4687b2d0108750fe4fb30eeec3f6c44bb689541e
Lin Chen <lc1990linux@gmail.com>
2020-01-10 22:52:08 +0800

    [zipkin-server][Chen] add zipkin server

---
+++ zipkin-server/src/main/java/com/clin/zipkinserver/DemoApplication.java
@@ -0,0 +1,16 @@
+package com.clin.zipkinserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import zipkin.server.EnableZipkinServer;
+
+@SpringBootApplication
+@EnableZipkinServer
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
