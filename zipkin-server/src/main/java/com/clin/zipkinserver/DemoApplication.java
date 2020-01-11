dda6f962f1cddbdcf2e2f13dd56842e1ecd117a7
Lin Chen <lc1990linux@gmail.com>
2020-01-11 23:21:13 +0800

    [ZipkinService][Chen] add ZipKin Stream server

--- zipkin-server/src/main/java/com/clin/zipkinserver/DemoApplication.java
+++ zipkin-server/src/main/java/com/clin/zipkinserver/DemoApplication.java
@@ -2,11 +2,11 @@ package com.clin.zipkinserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.sleuth.zipkin.stream.EnableZipkinStreamServer;
 
-import zipkin.server.EnableZipkinServer;
 
 @SpringBootApplication
-@EnableZipkinServer
+@EnableZipkinStreamServer
 public class DemoApplication {
 
 	public static void main(String[] args) {
