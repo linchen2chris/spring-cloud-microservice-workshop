77efdd11eee6acd91c497857d0ea6c9959f81c7f
Lin Chen <lc1990linux@gmail.com>
2020-01-13 14:48:18 +0800

    [ZipkinServer][Chen] fix zipkin server

--- zipkin-server/src/main/java/com/clin/zipkinserver/DemoApplication.java
+++ zipkin-server/src/main/java/com/clin/zipkinserver/DemoApplication.java
@@ -2,11 +2,10 @@ package com.clin.zipkinserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.sleuth.zipkin.stream.EnableZipkinStreamServer;
-
+import zipkin.server.EnableZipkinServer;
 
 @SpringBootApplication
-@EnableZipkinStreamServer
+@EnableZipkinServer
 public class DemoApplication {
 
 	public static void main(String[] args) {
